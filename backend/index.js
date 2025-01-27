b30c6abebdf829dea816ef1a029096320881d042
Basil-Raza-Khan <15062@.kiet.edu.pk>
2025-01-27 20:34:28 +0500

    Third  commit

--- backend/index.js
+++ backend/index.js
@@ -20,13 +20,13 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 const corsOperations = {
-    origin:'http://localhost:5173',
+    origin:'https://joblytics-8fdj.onrender.com',
     credentials:true
 }
 app.use(cors(corsOperations));
 // Alternatively, use this middleware for more control:
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Origin", "https://joblytics-8fdj.onrender.com");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
